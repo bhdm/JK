af006a7717af82f2ce207bf4643cf33a47d9225e
Maxim <bhd.m@ya.ru>
2015-10-21 18:43:50 +0300

    create project

---
+++ README.md
@@ -0,0 +1,4 @@
+jankol
+======
+
+A Symfony project created on October 21, 2015, 2:33 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @Template("AppBundle:Default:index.html.twig")
+     */
+    public function indexAction(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/contacts", name="contacts")
+     * @Template("AppBundle:Default:contacts.html.twig")
+     */
+    public function contactsAction(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/order", name="order")
+     * @Template("AppBundle:Default:order.html.twig")
+     */
+    public function orderAction(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/about", name="order")
+     * @Template("AppBundle:Default:about.html.twig")
+     */
+    public function aboutAction(Request $request)
+    {
+        return [];
+    }
+}
